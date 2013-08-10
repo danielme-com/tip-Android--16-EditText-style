0015c5169ff859e195960f56e129ae9583eec1f6
danielme.com <danielme_com@yahoo.com>
2013-08-10 20:10:14 +0200

    first commit

---
+++ src/com/danielme/tipsandroid/edittext/estilos/MainActivity.java
@@ -0,0 +1,23 @@
+package com.danielme.tipsandroid.edittext.estilos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import com.danieme.tipsandroid.edittext.estilos.R;
+
+/**
+ * 
+ * @author Daniel Medina (danielme.com)
+ *
+ */
+public class MainActivity extends Activity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{  
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);	
+	}
+}
